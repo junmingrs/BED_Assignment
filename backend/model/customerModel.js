e85515e7a5c538e9ee7481435d8fa6f0051a998a
Lu Sijin <Sijin5u>
2026-07-28 16:34:43 +0800

    display vendor email on stall page

--- backend/model/customerModel.js
+++ backend/model/customerModel.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const { poolPromise } = require("../db");
 
 const getCustomerByAccountId = async (accountId) => {
@@ -15,4 +16,18 @@ const getCustomerByAccountId = async (accountId) => {
     return result.recordset[0];
 };
 
+module.exports = { getAllCustomers, getCustomerByAccountId };
+=======
+// customerModel.js
+const { poolPromise } = require("../db");
+
+async function getCustomerByAccountId(accountId) {
+    const query = `SELECT customer_id, account_id, customer_name FROM Customer WHERE account_id = @accountId`;
+    const pool = await poolPromise;
+    const result = await pool.request().input("accountId", accountId).query(query);
+    if (result.recordset.length === 0) return null;
+    return result.recordset[0];
+}
+
 module.exports = { getCustomerByAccountId };
+>>>>>>> Stashed changes
